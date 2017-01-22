fcf6f5c9778fc824cf51700183976e49e08274b2
Saket Choudhary <saketkc@gmail.com>
2017-01-22 09:17:28 -0800

    conform to ENCODE api changes

--- encode_idr_peaks_download.py
+++ encode_idr_peaks_download.py
@@ -2,13 +2,71 @@ import os
 import requests
 import shutil
 import gzip
+import pprint
 import json
-from moca.helpers import safe_makedir
 
+pp = pprint.PrettyPrinter(indent=4)
 __base_url__ = 'https://www.encodeproject.org/'
-__root_dir__ = '/media/data1/ENCODE_V3/'
+__root_dir__ = '/media/dna/ENCODE_idr_20Jan_2017/'
 
 ALLOWED_FILETYPES = ['bed narrowPeak', 'bed broadPeak']
+def safe_makedir(dname):
+    """Make a directory if it doesn't exist, handling concurrent race conditions.
+
+    Credits: Brad Chapman for bcbio-nextgen: https://github.com/chapmanb/bcbio-nextgen/blob/master/bcbio/utils.py#L172
+
+    Parameters
+    ----------
+    dname: str
+        Path of directory to be created
+    """
+    if not dname:
+        return dname
+    num_tries = 0
+    max_tries = 5
+    while not os.path.exists(dname):
+        # we could get an error here if multiple processes are creating
+        # the directory at the same time. Grr, concurrency.
+        try:
+            os.makedirs(dname)
+        except OSError:
+            if num_tries > max_tries:
+                raise
+            num_tries += 1
+            time.sleep(2)
+    return dname
+
+def fetch_idr_record(metadata):
+    #pp.pprint(metadata)
+        #pp.pprint(metadata)
+    try:
+        files = metadata['files']
+        print(files)
+    except KeyError:
+        pass
+
+    biosample_term_name = metadata['biosample_term_name']
+    assay_term_name = metadata['assay_term_name']
+    description = metadata['description']
+    gene_name = metadata['gene_name']#['target']['label']
+    parent_metadata = {'biosample_term_name': biosample_term_name,
+                       'assay_term_name': assay_term_name,
+                       'description': description,
+                       'gene_name': gene_name}
+    idr_records = []
+    file_status = metadata['file_status']
+    file_type = metadata['file_type']
+    #output_type = metadata['output_type']
+
+    if file_type in ALLOWED_FILETYPES and file_status == 'released':
+        dataset = metadata['dataset']
+        dataset = dataset.replace('experiments','').replace('/','')
+        href = metadata['href']
+        title = metadata['peakfilename']
+        assembly = metadata['assembly']
+        idr_records.append({'href': href, 'metadata':metadata, 'parent_metadata': parent_metadata, 'dataset': dataset, 'peakfilename': title, 'assembly': assembly})
+    return idr_records
+
 
 def download_peakfile(source_url, filename, destination_dir):
     """Download peakfile from encode"""
@@ -52,6 +110,7 @@ def filter_metadata(metadata,
                     filter_dict={'files.output_type': 'optimal idr thresholded peaks'}):
     """Can think of supporting one nested key for now"""
     filter_keys = filter_dict.keys()[0].split('.')
+    #print(filter_keys)
     value = filter_dict.values()[0]
     files = metadata['files']
     biosample_term_name = metadata['biosample_term_name']
@@ -63,7 +122,8 @@ def filter_metadata(metadata,
         file_status = f['status']
         file_type = f['file_type']
         #output_type = f['output_type']
-        filter_1 = f[filter_keys[0]]
+        #print(f.keys())
+        #filter_1 = f[filter_keys[0]]
         filter_2 = f[filter_keys[1]]
         if filter_2 == value and file_type in ALLOWED_FILETYPES and file_status == 'released':
             dataset = f['dataset']
@@ -71,7 +131,6 @@ def filter_metadata(metadata,
             href = f['href']
             title = f['title']
             assembly = f['assembly']
-            print dataset
             filter_records.append({'href': href,
                                    'metadata':f,
                                    'parent_metadata': metadata,
@@ -87,7 +146,7 @@ def filter_metadata(metadata,
     return filter_records
 
 def search_encode_tfs():
-    url = __base_url__ + '/search?type=Experiment&assay_title=ChIP-seq&limit=all&status=released&target.investigated_as=transcription+factor&format=json'
+    url = __base_url__ + '/search?type=Experiment&assay_title=ChIP-seq&limit=all&status=released&target.investigated_as=transcription+factor&format=json'#&frame=embedded
     req = requests.get(url)
     resp_json = req.json()
     all_samples = resp_json['@graph']
@@ -95,9 +154,12 @@ def search_encode_tfs():
     all_tfs =  set(all_tfs)
     all_experiments = [sample['@id'].strip().replace('/','').replace('experiments', '') for sample in all_samples]
     for experiment in all_experiments:
+        print(experiment)
         expt_metadata = get_experiment(experiment)
         metadata = filter_metadata(expt_metadata)
-        download_idr_tfs(__root_dir__, metadata)
+        #download_idr_tfs(__root_dir__, metadata)
+        for m in metadata:
+            download_idr_tfs(__root_dir__, m)
 
 
 if __name__ == '__main__':
